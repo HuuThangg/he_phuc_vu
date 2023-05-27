056b36a970fc6533808b568c6bc14478dfb0ca25
HuuThangg <huuthang1215@gmail.com>
2023-05-27 22:36:11 +0700

    thêm api update tivi

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ server.get("/dsNguoidung",(req,res)=>{
     })
 })
 // Post
+// user
 server.post("/insertUser",(req,res)=>{
     let noi_dung_nhan=``;
     req.on("data",(dulieu)=>{
@@ -96,6 +97,83 @@ server.post("/deleteUser",(req,res)=>{
             "Ma_so": user.Ma_so
         }
         db.deleteOne("user",filter).then(result=>{
+            console.log(result);
+            res.json(ket_qua);
+        }).catch(err=>{
+            console.log(err);
+            ket_qua.noi_dung=false;
+            res.json(ket_qua);
+        })
+    })
+
+})
+
+// Tivi
+server.post("/insertTivi",(req,res)=>{
+    let noi_dung_nhan=``;
+    req.on("data",(dulieu)=>{
+        noi_dung_nhan+=dulieu;
+    })
+    req.on("end",()=>{
+        let tivi=JSON.parse(noi_dung_nhan);
+        let ket_qua={
+            noi_dung:true
+        }
+        db.insertOne("tivi",tivi).then(result=>{
+            console.log(result);
+            res.json(ket_qua)
+        }).catch(err=>{
+            console.log(err);
+            ket_qua.noi_dung=false;
+            res.json(ket_qua);
+        })
+    })
+})
+
+server.post("/updateTivi",(req,res)=>{
+    let noi_dung_nhan=``
+    let ket_qua={
+        noi_dung:true
+    }
+    req.on("data",(dulieu)=>{
+        noi_dung_nhan+= dulieu;
+
+    })
+    
+    req.on("end",()=>{
+        let tivi=JSON.parse(noi_dung_nhan);
+        let filter={
+            "Ma_so": tivi.Ma_so
+        }
+        let userUpdate={
+            "$set":{
+                "Don_gia_Ban":tivi.Don_gia_Ban 
+            }
+        }
+        db.updateOne("user",filter,userUpdate).then(result=>{
+            console.log(result);
+            res.json(ket_qua);
+        }).catch(err=>{
+            console.log(err);
+            ket_qua.noi_dung=false;
+            res.json(ket_qua);
+        })
+    })
+})
+server.post("/deleteTivi",(req,res)=>{
+    let noi_dung_nhan=``;
+    let ket_qua={
+        noi_dung:true
+    }
+    req.on("data",(dulieu)=>{
+        noi_dung_nhan+= dulieu
+    })
+    req.on("end",()=>{
+        let tivi=JSON.parse(noi_dung_nhan);
+        let filter={
+            "Ma_so": tivi.Ma_so
+        }
+        db.deleteOne("tivi",filter).then(result=>{
             console.log(result);
             res.json(ket_qua);
         }).catch(err=>{
